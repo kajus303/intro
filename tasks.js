e4ce9254e30fe4f75879d1f0a21fa309bb203ed5
kajus303 <kajus303@gmail.com>
2024-05-21 20:03:48 +0300

    ND

---
+++ tasks.js
@@ -0,0 +1,154 @@
+// ND - visos 128 kombinacijos
+
+console.log(true && true && true && true);
+console.log(true && true && true || true);
+console.log(true && true || true && true);
+console.log(true || true && true && true);
+console.log(true && true || true || true);
+console.log(true || true || true && true);
+console.log(true || true && true || true);
+console.log(true || true || true || true);
+console.log(true && true && true && false);
+console.log(true && true && true || false);
+console.log(true && true || true && false);
+console.log(true || true && true && false);
+console.log(true && true || true || false);
+console.log(true || true || true && false);
+console.log(true || true && true || false);
+console.log(true || true || true || false);
+console.log(true && true && false && true);
+console.log(true && true && false || true);
+console.log(true && true || false && true);
+console.log(true || true && false && true);
+console.log(true && true || false || true);
+console.log(true || true || false && true);
+console.log(true || true && false || true);
+console.log(true || true || false || true);
+console.log(true && false && true && true);
+console.log(true && false && true || true);
+console.log(true && false || true && true);
+console.log(true || false && true && true);
+console.log(true && false || true || true);
+console.log(true || false || true && true);
+console.log(true || false && true || true);
+console.log(true || false || true || true);
+console.log(false && true && true && true);
+console.log(false && true && true || true);
+console.log(false && true || true && true);
+console.log(false || true && true && true);
+console.log(false && true || true || true);
+console.log(false || true || true && true);
+console.log(false || true && true || true);
+console.log(false || true || true || true);
+console.log(true && true && false && false);
+console.log(true && true && false || false);
+console.log(true && true || false && false);
+console.log(true || true && false && false);
+console.log(true && true || false || false);
+console.log(true || true || false && false);
+console.log(true || true && false || false);
+console.log(true || true || false || false);
+console.log(true && false && false && true);
+console.log(true && false && false || true);
+console.log(true && false || false && true);
+console.log(true || false && false && true);
+console.log(true && false || false || true);
+console.log(true || false || false && true);
+console.log(true || false && false || true);
+console.log(true || false || false || true);
+console.log(false && false && true && true);
+console.log(false && false && true || true);
+console.log(false && false || true && true);
+console.log(false || false && true && true);
+console.log(false && false || true || true);
+console.log(false || false || true && true);
+console.log(false || false && true || true);
+console.log(false || false || true || true);
+console.log(false && true && true && false);
+console.log(false && true && true || false);
+console.log(false && true || true && false);
+console.log(false || true && true && false);
+console.log(false && true || true || false);
+console.log(false || true || true && false);
+console.log(false || true && true || false);
+console.log(false || true || true || false);
+console.log(true && false && true && false);
+console.log(true && false && true || false);
+console.log(true && false || true && false);
+console.log(true || false && true && false);
+console.log(true && false || true || false);
+console.log(true || false || true && false);
+console.log(true || false && true || false);
+console.log(true || false || true || false);
+console.log(false && true && false && true);
+console.log(false && true && false || true);
+console.log(false && true || false && true);
+console.log(false || true && false && true);
+console.log(false && true || false || true);
+console.log(false || true || false && true);
+console.log(false || true && false || true);
+console.log(false || true || false || true);
+console.log(true && false && false && false);
+console.log(true && false && false || false);
+console.log(true && false || false && false);
+console.log(true || false && false && false);
+console.log(true && false || false || false);
+console.log(true || false || false && false);
+console.log(true || false && false || false);
+console.log(true || false || false || false);
+console.log(false && false && false && true);
+console.log(false && false && false || true);
+console.log(false && false || false && true);
+console.log(false || false && false && true);
+console.log(false && false || false || true);
+console.log(false || false || false && true);
+console.log(false || false && false || true);
+console.log(false || false || false || true);
+console.log(false && false && true && false);
+console.log(false && false && true || false);
+console.log(false && false || true && false);
+console.log(false || false && true && false);
+console.log(false && false || true || false);
+console.log(false || false || true && false);
+console.log(false || false && true || false);
+console.log(false || false || true || false);
+console.log(false && true && false && false);
+console.log(false && true && false || false);
+console.log(false && true || false && false);
+console.log(false || true && false && false);
+console.log(false && true || false || false);
+console.log(false || true || false && false);
+console.log(false || true && false || false);
+console.log(false || true || false || false);
+console.log(false && false && false && false);
+console.log(false && false && false || false);
+console.log(false && false || false && false);
+console.log(false || false && false && false);
+console.log(false && false || false || false);
+console.log(false || false || false && false);
+console.log(false || false && false || false);
+console.log(false || false || false || false);
+
+/*
+Kitas budas:
+
+0000
+0001
+0010
+0011
+0100
+0101
+0110
+0111
+1000
+1001
+1010
+1011
+1100
+1101
+1110
+1111
+*/
+
+console.clear();
+
